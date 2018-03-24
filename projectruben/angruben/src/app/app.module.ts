6ea5e883111d2760c7798a5f80d15df99e1fc76d
woodi3 <wawood@email.sc.edu>
2018-03-24 03:24:17 -0400

    Some content for the home page

--- projectruben/angruben/src/app/app.module.ts
+++ projectruben/angruben/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes =  [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
